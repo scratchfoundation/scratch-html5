2350abf3d49b001b2463e2d44a0784c0c862b69c
William Ferguson <wferguson@scottlogic.com>
2014-07-25 11:16:34 +0100

    Fix: PID 17828009 23322186 Issues https://github.com/WPFerg/scratch-html5/issues/6 https://github.com/WPFerg/scratch-html5/issues/5

--- js/IO.js
+++ js/IO.js
@@ -112,7 +112,7 @@ IO.prototype.makeObjects = function() {
             }
             return newSprite;
         }
-        if (obj.objName) { // sprite
+        if (typeof(obj.objName) === "string") { // sprite
             newSprite = createSprite(obj);
             sprite = newSprite;
         } else {
